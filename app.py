f47ad196a977916fc6c9bcb57bee88d532cc39f8
Taneem Jan <taneemishere@gmail.com>
2020-11-14 09:30:34 +0500

    Some fine tuning in the code!

--- app.py
+++ app.py
@@ -1,38 +1,62 @@
-import my_training
-import pickle
 from flask import Flask, render_template, request
+import pandas as pd
+# import numpy as np
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import MultinomialNB
+import pickle
 
 app = Flask(__name__)
 
-# TODO: 2. if todo 1 is not working then these lines for file opening and importing pickle is BEKAAAAR!
-# Opening the file
-file = open("NBC_Model_final.pkl", "rb")
-clf = pickle.load(file=file)
-file.close()
 
-
-@app.route('/', methods=["GET", "POST"])
-def predict():
-    if request.method == "POST":
-        comment = request.form['predict_comment']
-        data = [comment]
-        vectorizer_predict = my_training.vectorizer.transform(data).toarray()
-
-        model_predict = my_training.clf.predict(vectorizer_predict)
-
-        # TODO:
-        #  1. this has to be checked after implementing the front whether is working not
-        # model_predict = clf.predict(vectorizer_predict)
-        print(model_predict)
-
-        if model_predict == 1:
-            prediction = "Spam"
-            return render_template('show.html', prediction=prediction)
-        elif model_predict == 0:
-            prediction = "Not Spam/ Ham"
-            return render_template('show.html', prediction=prediction)
-
+@app.route('/')
+def index():
     return render_template('index.html')
+
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    load_data = pd.read_csv('final_dataframe.csv')
+    data_df = load_data[["CONTENT", "CLASS"]]
+
+    # Our features and label
+    data_feature = data_df['CONTENT']
+    data_label = data_df['CLASS']
+
+    # Extracting features from text
+    corpus = data_feature
+    vectorizer = CountVectorizer()
+
+    # Fit the data (feature) which is in corpus
+    X = vectorizer.fit_transform(corpus)
+
+    # Splitting the data into training and testing
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, data_label, test_size=0.33, random_state=42
+    )
+
+    # Building the model
+    clf = MultinomialNB()
+    clf.fit(X_train, y_train)
+
+    clf.score(X_test, y_test)
+
+    print("Model Score is: ", clf.score(X_test, y_test))
+
+    if request.method == "POST":
+        predict_comment = request.form['predict_comment']
+        data = [predict_comment]
+        print("The received comment is: ", data)
+        vectorizer_predict = vectorizer.transform(data).toarray()
+        model_prediction = clf.predict(vectorizer_predict)
+
+        if model_prediction == [1]:
+            model_prediction = "Spam"
+
+        elif model_prediction == [0]:
+            model_prediction = "Not Spam/ Ham"
+
+    return render_template('show.html', prediction=model_prediction)
 
 
 if __name__ == '__main__':
